93e46d26b1c687028f20d0f27e92e375ff71eb39
Fredrik Thulin <extern.fredrik.thulin@digg.se>
2026-01-20 17:31:05 +0100

    add unit tests
    Signed-off-by: Fredrik Thulin <extern.fredrik.thulin@digg.se>

--- rust-r2ps-worker/src/application/helpers.rs
+++ rust-r2ps-worker/src/application/helpers.rs
@@ -29,3 +29,34 @@ impl AsRef<[u8]> for ByteVector {
         &self.0
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn debug_works() {
+        let bv = ByteVector::new(vec![0xaa, 0x55, 0x00, 0xbb]);
+        // verify that fmt::Debug works
+        assert_eq!(format!("{:?}", bv), "ByteVector(aa5500bb)");
+    }
+
+    #[test]
+    fn deref_works() {
+        let bv = ByteVector::new(vec![1, 2, 3]);
+
+        // Test deref coercion to slice
+        let slice: &[u8] = &bv;
+        assert_eq!(slice, &[1, 2, 3]);
+    }
+
+    #[test]
+    fn as_ref_works() {
+        let bv = ByteVector::new(vec![4, 5, 6]);
+
+        // Test AsRef trait
+        let as_ref: &[u8] = bv.as_ref();
+        assert_eq!(as_ref, &[4, 5, 6]);
+    }
+}
